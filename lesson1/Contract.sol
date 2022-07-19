738f7cec68267e32e92a507e49e715568427ad71
infifab <tmtg360@gmail.com>
2022-07-19 14:12:49 +0530

    first lesson completed

---
+++ lesson1/Contract.sol
@@ -0,0 +1,40 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
+
+
+// by convention private funtions start with _
+// memory is added while declaring arguments for string, array, struct etc
+// declaring arguments for functions add _ before argument
+// function %name%(%args%) %public/private% %/view/pure% returns(%data type%)
+//    -- view: when you are viewing data from the smart contract.
+//    -- pure: pass by value, returns some value or not but has nothing to do with the values in smart contract 
